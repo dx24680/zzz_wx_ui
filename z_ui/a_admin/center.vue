b8bf204bab3a9ef4cdb4a4a677e61f7a5d7b0d7b
dx24680 <76042006+dx24680@users.noreply.github.com>
2022-02-22 14:35:20 +0800

    Add files via upload

---
+++ z_ui/a_admin/center.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="center">
+        <!-- center中转页 -->
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function ()
+        {
+            return {
+                center_url: ""
+            };
+        },
+        created()
+        {
+            console.log("ct center", func_get_cookie("center_url"))
+            this.center_url = func_get_cookie("center_url")
+            // 二维码扫完都会来这里中转，存首页id
+            let center_id = this.center_url.split(`/${g_z_ui_dir}/a_admin/center_`)[1]
+            func_set_cookie("center_id", center_id)
+            this.$router.push({path: this.center_url})
+        },
+        mounted()
+        {
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped>
+</style>
---
+++ z_ui/a_admin/center_re.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="center_re">
+        <!-- <a target="iframe_app" :href="iframe_host + app_url"
+          ><span class="btn_a"></span
+        ></a>
+        <iframe
+          class="iframe_app"
+          src="about:blank"
+          id="iframe_app"
+          name="iframe_app"
+          frameborder="0"
+        ></iframe> -->
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function ()
+        {
+            return {
+                iframe_host: g_config.api_root + "/zz_admin/",
+            };
+        },
+        created()
+        {
+            console.log("created center_re", this.$route);
+            setTimeout(() =>
+            {
+                let url_res
+                if (this.$route.query.app_url)
+                {
+                    if(g_z_ui_dir == 'z_ui_user'){
+                        location.replace(`app.html#/admin?&tokenAdmin=${func_get_cookie("token_qr")}&site_id=${func_get_cookie("site_id")}&site_id_alias=${func_get_cookie("site_id_alias")}&app_id=${this.$route.query.app_id}&path=${this.$route.query.app_url}`)
+                    }else{
+                        url_res = `${this.$route.query.app_url}&tokenAdmin=${func_get_cookie("token_qr")}`
+                        console.log("center_re url_res", this.iframe_host + url_res)
+                        location.replace(this.iframe_host + url_res)
+                    }
+                }
+                else if (this.$route.query.url)
+                {
+                    url_res = `${this.$route.query.url}&tokenAdmin=${func_get_cookie("token_qr")}`
+                    location.replace(url_res)
+                }
+            }, 1000)
+
+
+        },
+        mounted()
+        {
+            $(".btn_a").click()
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped>
+    .iframe_app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
